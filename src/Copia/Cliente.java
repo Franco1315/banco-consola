931c6856021b014101a7e14e70dc5e90b1f4d621
Maria_franco <chiqui0205@gmail.com>
2023-07-07 11:33:34 +0200

    codigo practica 3

---
+++ src/Copia/Cliente.java
@@ -0,0 +1,11 @@
+package Copia;
+
+
+
+public class Cliente{
+
+	public Cliente() {
+		
+	}
+
+}
---
+++ src/banco/model/Gestor.java
@@ -0,0 +1,39 @@
+package banco.model;
+
+
+public class Gestor extends Usuario {
+
+	private String oficina;
+
+	public Gestor() {
+	}
+
+	public Gestor(Integer id, String usuario, String password, String correo) {
+		super(id, usuario, password, correo);
+	}
+
+	public Gestor(Integer id, String usuario, String password, String correo, String oficina) {
+		super(id, usuario, password, correo);
+		this.oficina = oficina;
+	}
+
+	public String getOficina() {
+		return oficina;
+	}
+
+	public void setOficina(String oficina) {
+		this.oficina = oficina;
+	}
+
+	public String toString() {
+		return "Gestor{id: " + this.id + ", nombre: " + this.usuario + ", oficina: " + this.oficina + "}";
+	}
+
+
+
+	public String getPassword() {
+		
+		return null;
+	}
+
+}
